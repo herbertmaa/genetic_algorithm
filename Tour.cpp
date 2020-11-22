4a61a819cb0ebe7efc2ab50d0a2f1df189c0244f
herbertma <hma49@sfu.ca>
2020-11-22 11:33:12 -0800

    small update to algo

--- Tour.cpp
+++ Tour.cpp
@@ -17,16 +17,14 @@ constexpr int ITERATIONS = 1000;
 constexpr int MAP_BOUNDARY = 1000;
 constexpr int PARENT_POOL_SIZE = 5;
 constexpr double MUTATION_RATE = 0.15;
-constexpr int NUMBER_OF_PARENTS = 1; //TODO ? What is this used for ?
 constexpr int NUMBER_OF_ELITES = 1;
 
 Tour::Tour(int num_cities) {
-
     std::mt19937 eng{RANDOM_SEED};
     std::uniform_int_distribution<int> dist{City::min_range, City::max_range};
 
-    for(int i = 0; i < num_cities; i++){
-        City* c = new City(dist(eng), dist(eng), "C" + std::to_string(sequence_number));
+    for (int i = 0; i < num_cities; i++) {
+        City *c = new City(dist(eng), dist(eng), "C" + std::to_string(sequence_number));
         ++sequence_number;
         cities.push_back(c);
     }
@@ -35,7 +33,7 @@ Tour::Tour(int num_cities) {
 
 ostream &operator<<(ostream &os, const Tour &t) {
 
-    for(auto it = t.cities.begin(); it != t.cities.end(); ++it){
+    for (auto it = t.cities.begin(); it != t.cities.end(); ++it) {
         os << **it;
     }
     return os;
@@ -48,7 +46,7 @@ void Tour::shuffle_cities() {
 
 
 Tour::Tour(const Tour &t) {
-    for(auto it = t.cities.begin(); it != t.cities.end(); ++it){
+    for (auto it = t.cities.begin(); it != t.cities.end(); ++it) {
         cities.push_back((*it));
     }
     this->fitness = determine_fitness();
@@ -56,15 +54,19 @@ Tour::Tour(const Tour &t) {
 
 double Tour::determine_fitness() {
     double total_distance = 0;
-    for(auto it = cities.begin(); it != cities.end()-1; ++it){
-        total_distance = get_distance_between_cities(**it, **(it+1));
+    for (auto it = cities.begin(); it != cities.end() - 1; ++it) {
+        total_distance = get_distance_between_cities(**it, **(it + 1));
     }
+
+    if (total_distance == 0) throw std::invalid_argument("DIVIDING BY ZERO");
+
     return 1 / total_distance * RANDOM_SEED;
 }
 
-bool operator<(const Tour &t1, const Tour &t2) {
-    return (t1.fitness < t2.fitness);
+bool Tour::operator<(const Tour &t2) const {
+    return (this->fitness < t2.fitness);
 }
+
 
 void Tour::mutation() {
     std::random_device rd;
@@ -72,5 +74,14 @@ void Tour::mutation() {
     auto it = cities.begin() + d(rd);
     auto it2 = cities.begin() + d(rd);
     std::iter_swap(it, it2);
+    this->fitness = determine_fitness();
 }
+
+
+Tour &Tour::operator=(Tour assignment) {
+    cities = assignment.cities; // assigns all pointers in deque to point to the values in assignment.cities
+    fitness = assignment.fitness; // assigns the fitness of the tour to the fitness of the assignment tour
+    return *this;
+}
+
 
--- Tour.hpp
+++ Tour.hpp
@@ -11,21 +11,26 @@ using std::ostream;
 
 class Tour {
 private:
-    double improvement_factor;
+    double improvement_factor = 0;
     std::vector<City* > cities;
     int sequence_number = 0;
     double fitness = 0;
     double determine_fitness();
 public:
     Tour(int num_cities);
+    Tour() = default;
+    Tour& operator=(Tour assignment);
     Tour(const Tour&t);
     void shuffle_cities();
-    void select_parents();
     bool contains_city(City* city);
     void mutation();
+    inline int num_cities() const { return this->cities.size(); };
     inline double get_fitness() const {return this->fitness;};
     friend ostream&operator <<(ostream&os, const Tour& t);
     constexpr static int CITIES_IN_TOURS = 10;
-    friend bool operator <(const Tour& t1, const Tour& t2);
+    bool operator < (const Tour& t2) const;
+    void insert_city_front(City * city);
+    void insert_city_back(City * city);
+
 };
 
