b814fbd23bad60321c7c02d48b8df0ff83632c7f
herbertma <hma49@sfu.ca>
2020-11-24 23:12:53 -0800

    added more comments to cityList

--- impl/CityList.cpp
+++ impl/CityList.cpp
@@ -9,20 +9,24 @@
 
 
 CityList &CityList::get_instance() {
-    // Create only one instance inside this method. This is the instance that we will access. FOREVER.
+
+    // Create only one instance inside this method. This is the instance that we will access.
     static CityList instance;
 
+    //Initialize the cities within the Singleton object, this only happens once.
     if(instance.cities.empty()){
-        std::mt19937 eng{RANDOM_SEED};
-        std::uniform_int_distribution<int> dist{City::MIN_RANGE, City::MAX_RANGE};
+        std::mt19937 e{std::random_device{}()};
+        std::uniform_int_distribution<int> dist{City::MIN_MAP_BOUNDARY, City::MAX_MAP_BOUNDARY};
         for (int i = 0; i < CITIES_IN_TOUR; ++i) {
-            instance.cities.push_back(new City(dist(eng), dist(eng), "C" + std::to_string(i)));
+            instance.cities.push_back(new City(dist(e), dist(e), "C" + std::to_string(i)));
         }
     }
     return instance;
 }
 
 CityList::~CityList() {
+
+    // Deallocate City memory that was made when we created the instance of the Singleton
     for (auto it = cities.begin(); it != cities.end(); it++) {
         delete (*it);
     }
@@ -30,8 +34,12 @@ CityList::~CityList() {
 
 
 vector<City *> CityList::shuffle() const {
+    // Create a copy of the cities currently in the Singleton
     vector<City *> copy_cities = cities;
+
+    // Use STL shuffle to shuffle the copy
     std::mt19937 e{std::random_device{}()};
     std::shuffle(copy_cities.begin(), copy_cities.end(), e);
+
     return copy_cities;
 }
